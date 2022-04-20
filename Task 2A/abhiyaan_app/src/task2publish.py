d852a1d342818eb4ef1b0141e1ea23112718fc2a
saiharan12 <saiharan12@gmail.com>
2022-04-20 21:09:27 +0530

    Update task2publish.py
    Commented

--- Task 2A/abhiyaan_app/src/task2publish.py
+++ Task 2A/abhiyaan_app/src/task2publish.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-import rospy 
-from std_msgs.msg import String
-rospy.init_node('pub') 
-pub = rospy.Publisher('team_abhiyaan', String) 
-rate = rospy.Rate(2)
-while not rospy.is_shutdown(): 
-	pub.publish("Team Abhiyaan rocks:") 
-	rate.sleep()
+import rospy #import the library
+from std_msgs.msg import String #import String message datatype
+rospy.init_node('pub') #initialise node
+pub = rospy.Publisher('team_abhiyaan', String) #declare publisher
+rate = rospy.Rate(2) #set message rate
+while not rospy.is_shutdown(): #runs only until it's not stopped, or roscore is closed
+	pub.publish("Team Abhiyaan rocks:") #publish the message
+	rate.sleep() #delay
