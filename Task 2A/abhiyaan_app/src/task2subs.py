d289e33ca571db8dd9d16b31fd6c441b4e35bc4e
saiharan12 <saiharan12@gmail.com>
2022-04-20 21:15:44 +0530

    Update task2subs.py

--- Task 2A/abhiyaan_app/src/task2subs.py
+++ Task 2A/abhiyaan_app/src/task2subs.py
@@ -6,4 +6,4 @@ def callback(msg): #called when subscriber receives a message
 rospy.init_node('subs') #initialises node
 sub1 = rospy.Subscriber('team_abhiyaan', String, callback) # Subscibes to the topic normal string is publishing to 'team_abhiyaan'
 sub2 = rospy.Subscriber('naayihba_maet', String, callback) # Also subscribes to the reversed string topic 'naayihba_maet'
-rospy.spin()
+rospy.spin() #keeps the code running
