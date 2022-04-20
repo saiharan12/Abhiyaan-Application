97b5b23f44982cc065ce9b4623479671ed9ea02e
saiharan12 <saiharan12@gmail.com>
2022-04-20 21:15:32 +0530

    Update task2subs.py
    Commented

--- Task 2A/abhiyaan_app/src/task2subs.py
+++ Task 2A/abhiyaan_app/src/task2subs.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-import rospy 
-from std_msgs.msg import String
-def callback(msg): 
-	print (msg.data)
-rospy.init_node('subs') 
-sub1 = rospy.Subscriber('team_abhiyaan', String, callback)
-sub2 = rospy.Subscriber('naayihba_maet', String, callback)
+import rospy #imports the library 
+from std_msgs.msg import String #imports String message datatype
+def callback(msg): #called when subscriber receives a message
+	print (msg.data) #prints the data in the message received
+rospy.init_node('subs') #initialises node
+sub1 = rospy.Subscriber('team_abhiyaan', String, callback) # Subscibes to the topic normal string is publishing to 'team_abhiyaan'
+sub2 = rospy.Subscriber('naayihba_maet', String, callback) # Also subscribes to the reversed string topic 'naayihba_maet'
 rospy.spin()
